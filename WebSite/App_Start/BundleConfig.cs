5c9bbf643b6f8e59b8a39b40bf5e50b93d8b0484
happyTanh <huan_tan@qq.com>
2014-02-21 17:47:38 +0800

    happyTan网站的基本架构
    bootstrap样式框架、ASP.NET MVC架构

---
+++ WebSite/App_Start/BundleConfig.cs
@@ -0,0 +1,21 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace WebSite
+{
+    public class BundleConfig
+    {
+        // 有关 Bundling 的详细信息，请访问 http://go.microsoft.com/fwlink/?LinkId=254725
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/Scripts/jquery").Include(
+                        "~/Scripts/jquery-1.8.2.min.js"));
+
+            bundles.Add(new ScriptBundle("~/Content/bootstrapjs").Include(
+                        "~/Content/bootstrap/js/bootstrap.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/bootstrapcss").Include(
+                        "~/Content/bootstrap/css/bootstrap.min.css"));
+        }
+    }
+}
